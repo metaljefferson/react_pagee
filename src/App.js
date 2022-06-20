2aa1818d5ded155795b328657850bc0210d56a6f
Jefferson Paiva <jefferson.paiva@MAISVALOR.COM>
2022-06-20 17:49:36 -0300

    Manutençao da landing Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Portfolio from "./components/Portfolio";
 import "./App.css";
-import logo from "./assets/img/logo-maisvalor-branco.png";
+
 
 
 
@@ -52,7 +52,7 @@ function App() {
         id="mainNav"
       >
         <div className="container">
-          <img className="img-fluid  mx-auto" src={logo} alt="" />
+          <img className="img-fluid  mx-auto" src={images['logo-maisvalor-branco.png']} alt="" />
           <button
             className="navbar-toggler navbar-toggler-right"
             type="button"
@@ -393,48 +393,160 @@ function App() {
         </div>
       </section>
 
-      <section className="py-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 col-sm-6">
-              <a href="#something">
-                <img
-                  className="img-fluid d-block mx-auto"
-                  src="img/logos/envato.jpg"
-                  alt=""
-                />
-              </a>
+      <h2 className="text-center m-3">Nossos Parceiros:</h2>
+
+      <div className="container card-group  w-60">
+        <div className="accordion  d-lg-flex" id="accordion">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="one">
+              
+              <button
+               className="accordion-button btn"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-one"
+                aria-expanded="true"
+                aria-controls="collapse-one"
+              >
+                <img className="card-img-top" src={images['cnpj.png']}></img>
+              </button>
+            </h2>
+            <div
+              id="collapse-one"
+              className="accordion-collapse collapse"
+              aria-labelledby="one"
+              data-bs-parent="#accordion"
+            >
+              <div className="accordion-body">
+                <p>This is accordion body.</p>
+              </div>
             </div>
-            <div className="col-md-3 col-sm-6">
-              <a href="#something">
-                <img
-                  className="img-fluid d-block mx-auto"
-                  src="img/logos/designmodo.jpg"
-                  alt=""
-                />
-              </a>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="two">
+              <button
+                className="accordion-button collapsed btn"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-two"
+                aria-expanded="false"
+                aria-controls="collapse-two"
+              >
+              <img className="img-fluid card-img-top" src={images['cnpj.png']}></img>
+              </button>
+            </h2>
+            <div
+              id="collapse-two"
+              className="accordion-collapse collapse"
+              aria-labelledby="two"
+              data-bs-parent="#accordion"
+            >
+              <div className="accordion-body">
+                <p>This is accordion body.</p>
+              </div>
             </div>
-            <div className="col-md-3 col-sm-6">
-              <a href="#something">
-                <img
-                  className="img-fluid d-block mx-auto"
-                  src="img/logos/themeforest.jpg"
-                  alt=""
-                />
-              </a>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="three">
+              <button
+                className="accordion-button collapsed btn"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-three"
+                aria-expanded="false"
+                aria-controls="collapse-three"
+              >
+              <img className="img-fluid card-img-top" src={images['cnpj.png']}></img>
+              </button>
+            </h2>
+            <div
+              id="collapse-three"
+              className="accordion-collapse collapse"
+              aria-labelledby="three"
+              data-bs-parent="#accordion"
+            >
+              <div className="accordion-body">
+                <p>This is accordion body.</p>
+              </div>
             </div>
-            <div className="col-md-3 col-sm-6">
-              <a href="#something">
-                <img
-                  className="img-fluid d-block mx-auto"
-                  src="img/logos/creative-market.jpg"
-                  alt=""
-                />
-              </a>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="four">
+              <button
+                className="accordion-button collapsed btn"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-four"
+                aria-expanded="false"
+                aria-controls="collapse-four"
+              >
+              <img className="img-fluid card-img-top" src={images['cnpj.png']}></img>
+              </button>
+            </h2>
+            <div
+              id="collapse-four"
+              className="accordion-collapse collapse"
+              aria-labelledby="four"
+              data-bs-parent="#accordion"
+            >
+              <div className="accordion-body">
+                <p>This is accordion body.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="five">
+              <button
+                className="accordion-button collapsed btn"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-five"
+                aria-expanded="false"
+                aria-controls="collapse-five"
+              >
+              <img className="img-fluid card-img-top" src={images['cnpj.png']}></img>
+              </button>
+            </h2>
+            <div
+              id="collapse-five"
+              className="accordion-collapse collapse"
+              aria-labelledby="five"
+              data-bs-parent="#accordion"
+            >
+              <div className="accordion-body">
+                <p>This is accordion body.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="six">
+              <button
+                className="accordion-button collapsed btn"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-six"
+                aria-expanded="false"
+                aria-controls="collapse-six"
+              >
+              <img className="img-fluid card-img-top" src={images['cnpj.png']}></img>
+              </button>
+            </h2>
+            <div
+              id="collapse-six"
+              className="accordion-collapse collapse"
+              aria-labelledby="six"
+              data-bs-parent="#accordion"
+            >
+              <div className="accordion-body">
+                <p>This is accordion body.</p>
+              </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
 
       <section className="page-section" id="contact">
         <div className="container">
@@ -509,55 +621,139 @@ function App() {
                     </button>
                   </div>
                 </div>
-                
               </form>
             </div>
           </div>
         </div>
       </section>
 
-      <footer className="footer">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-md-4">
-              <span className="copyright">
-                Copyright &copy; 2022 Grupo Mais Valor
-              </span>
-            </div>
-            <div className="col-md-4">
-              <ul className="list-inline social-buttons">
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa-brands fa-twitter"></i>
+      <footer className="text-center text-lg-start">
+        <div className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <div className="me-5 d-none d-lg-block">
+            <span>Conecte-se conosco nas redes sociais</span>
+          </div>
+
+          <div className="icon_networks">
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-google"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">
+                  <i className="fas fa-gem me-3"></i>Grupo Mais Valor
+                </h6>
+                <p>
+                  A Mais Valor é uma das maiores gestoras de negócios,
+                  especializada em assessoria financeira para pessoas físicas e
+                  empresas, com agências e representantes distribuídos pelos
+                  principais estados do Brasil.
+                </p>
+              </div>
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Produtos</h6>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Angular
                   </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa-brands fa-facebook"></i>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    React
                   </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">
-                    <i className="fa-brands fa-linkedin-in"></i>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Vue
                   </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <ul className="list-inline quicklinks">
-                <li className="list-inline-item">
-                  <a href="#something">Privacy Policy</a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#something">Terms of Use</a>
-                </li>
-              </ul>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Laravel
+                  </a>
+                </p>
+              </div>
+
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Sobre Nós</h6>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Pricing
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Settings
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Orders
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Help
+                  </a>
+                </p>
+              </div>
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Contato</h6>
+                <p>
+                  <i className="fas fa-home me-3"></i> Rua Quinze de Novembro
+                  184 Sala 601, Centro, SP, 01013-904
+                </p>
+                <p>
+                  <i className="fas fa-envelope me-3"></i>
+                  sac@grupomaisvalor.com.br
+                </p>
+                <p>
+                  <i className="fas fa-phone me-3"></i> (11) 3467-0070
+                </p>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div
+          className="text-center p-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        >
+          Copyright © 2019/2022 - Grupo Mais Valor. Todos os direitos reservados
         </div>
       </footer>
     </div>
   );
 }
+
+function importAll(r) {
+  let images = {};
+   r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images
+ }
+
+ const images = importAll(require.context('./assets/img/', false, /\.(png|jpe?g|svg)$/));
+ console.log(images)
 
 export default App;
